ead4c76f054ed038c32495d78fc8462379fa2cb7
aloks <alok.singh@externals.hilti.com>
2023-04-29 12:08:33 +0530

    di added

---
+++ app/src/main/java/com/doersweb/calorietracker/di/AppModule.kt
@@ -0,0 +1,33 @@
+package com.doersweb.calorietracker.di
+
+import android.app.Application
+import android.content.Context.MODE_PRIVATE
+import android.content.SharedPreferences
+import com.doersweb.core.data.UserPreferences
+import com.doersweb.core.domain.preferences.Preferences
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Provides
+    @Singleton
+    fun providePreferences(
+        sharedPreferences: SharedPreferences
+    ): Preferences {
+        return UserPreferences(sharedPreferences)
+    }
+
+    @Provides
+    @Singleton
+    fun provideSharedPreferences(
+        app: Application
+    ): SharedPreferences {
+        return app.getSharedPreferences("shared_pref", MODE_PRIVATE)
+    }
+}
